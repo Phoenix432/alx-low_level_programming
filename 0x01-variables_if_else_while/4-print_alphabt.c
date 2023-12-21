09599bdd4411c1f9abfb488e240a8072557282bd
Phoenix432 <abdellahouben95@gmail.com>
2023-12-20 20:44:49 -0800

    0x01 C Variables Task 4 edited

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,9 +14,10 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if (ch == 'e' || ch == 'q')
-			ch++;
-		putchar(ch);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
 		ch++;
 	}
 	putchar('\n');
