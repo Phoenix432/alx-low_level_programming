46c26d42164ed34c709ba55ad5e7d8153f943c04
Phoenix432 <abdellahouben95@gmail.com>
2024-01-03 16:51:50 -0800

    0x02-C-Functions nested loops Task 6 - edited

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,28 @@
 #include "main.h"
 
 /**
- * _abs - function that computes the absolute
- *       value of an integer
- *
- * @n: takes in integer type input for function
- *
- * Return: Always 0 (Success)
+* _abs - function that computes the absolute
+*       value of an int
+*
+* @n: int input
+*
+* Return: Always 0 (Success)
 */
 
 int _abs(int n)
 {
 	if (n < 0)
-		n = (-1) * n;
+	{
+	return (0 - n);
+	}
+	else
+	{
 	return (n);
+	}
 }
+
+
+
+
+
+
