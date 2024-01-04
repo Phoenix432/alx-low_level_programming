eb9d02c18ffb05c005490620665ea89f377055aa
Phoenix432 <abdellahouben95@gmail.com>
2024-01-04 15:56:24 -0800

    0x02-C-Functions nested loops Task 12 - edited

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,55 @@
-#include "main.h"
+#include<stdio.h>
+
+
 
 /**
- * print_times_table - prints time table of n
- *
- * @n: takes number input
+* print_times_table - prints the n time table
+*
+* @n: take number
+*
 */
 
 void print_times_table(int n)
 {
-	int prod, mult, num;
+	int i, j, prod;
 
 	if (n <= 15 && n >= 0)
 	{
-		for (num = 0; num <= n; num++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar(48);
-			for (mult = 1; mult <= n; mult++)
+			for (j = 1; j <= n; j++)
 			{
+				prod = i * j;
+
 				_putchar(',');
 				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod <= 99)
-					_putchar(' ');
-
-				if (prod >= 100)
+				if (prod > 9 && prod < 100)
 				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
-				} else if (prod <= 99 && prod >= 10)
+					_putchar(' ');
 					_putchar((prod / 10) + 48);
+				}
+				else if (prod > 99)
+				{
+					_putchar((prod / 100)  + 48);
+					_putchar(((prod / 10) % 10) + 48);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
 	}
 }
+
+
+
+
+
+
+
+
