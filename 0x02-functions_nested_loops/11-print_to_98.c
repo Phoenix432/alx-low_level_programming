e1c08679163e7901f913a6519fa292e04b1a40b0
Phoenix432 <abdellahouben95@gmail.com>
2024-01-04 08:31:51 -0800

    0x02-C-Functions nested loops Task 11 - edited

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,48 @@
-#include "main.h"
+#include"main.h"
+
 
 /**
- * print_to_98 - print n to 98 counts
+* print_to_98 - print n to 98 counts
  *           separated by comma, followed
  *           by space and number should be
  *           printed in order
  *
- * @n: input
+* @n : input
+*
 */
 
 void print_to_98(int n)
 {
-	int count;
+	int i;
 
-	if (n > 98)
-		for (count = n; count > 98; count--)
-			printf("%d, ", count);
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+				printf(", ");
+		}
+	}
 	else
-		for (count = n; count < 98; count++)
-			printf("%d, ", count);
-	printf("98\n");
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+				printf(", ");
+
+		}
+	}
+
+printf("\n");
 }
+
+
+
+
+
+
+
