fab36ff60b01e8eb5c05683c915c8d161462bfe2
Phoenix432 <abdellahouben95@gmail.com>
2024-01-03 16:23:40 -0800

    0x02-C-Functions nested loops Task 4 - edited

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,16 +4,19 @@
  * _isalpha - checks if character is a letter
  *            both lowercase or uppercase
  *
- * @c: takes input from other functions.
+ * @c: input to check
  *
- * Return: 1 is c if true else 0
+ * Return: 1 if c alphabetic 0 otherwise
 */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
-		return (1);
-	if (c >= 65 && c <= 90)
-		return (1);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+	return (1);
+	}
+	else
+	{
 	return (0);
+	}
 }
