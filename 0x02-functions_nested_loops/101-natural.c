5de276e2f30c54874c1e9b820d0f11db48b2f87a
Phoenix432 <abdellahouben95@gmail.com>
2024-01-04 16:37:10 -0800

    0x02-C-Functions nested loops Task 13 - edited

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,8 +14,9 @@ int main(void)
 {
 	int limit = 1024;
 	int sum = 0;
+	int i;
 
-	for (int i = 0; i < limit; i++)
+	for (i = 0; i < limit; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
