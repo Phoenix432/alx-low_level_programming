41ea9aec7e4ca58132d3a9e0efa1e1d6e97b242a
Phoenix432 <abdellahouben95@gmail.com>
2024-01-06 19:09:47 -0800

    0x04 C More functions more nested loops Task 5 e

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,16 +2,18 @@
 
 /**
  * more_numbers - print 0 - 14 ten times and you
- *		  putchar three times
+ *		 _putchar three times
  *
  * Return: Always 0 (Success)
 */
 
 void more_numbers(void)
 {
-	for (int j = 1; j <= 10; j++)
+	int i, j;
+
+	for (j = 1; j <= 10; j++)
 	{
-		for (int i = 0; i <= 14; i++)
+		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
 			{
