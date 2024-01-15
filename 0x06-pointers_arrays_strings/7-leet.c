c25cf63860bfb580c8d9c349cae4b476f894e4e5
Phoenix432 <abdellahouben95@gmail.com>
2024-01-15 07:35:42 -0800

    0x06.C-More pointers, arrays and strings T7 e

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,30 +2,30 @@
 
 /**
  * leet - encodes a string into 1337
- * @c: String
+ * @s: String
  * Return: string that is encoded
 */
 
-char *leet(char *c)
+char *leet(char *s)
 {
-	char *cp = c;
-	char key[] = {'A', 'E', 'O', 'T', 'L'};
-	int value[] = {4, 3, 0, 7, 1};
-	unsigned int i;
+	char *ptr = s;
+	char s1[] = "43071";
+	char s2[] = "aeotl";
+	char s3[] = "AEOTL";
+	int i = 0, j;
 
-	while (*c)
+	while (s[i])
 	{
-		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		j = 0;
+		while (j < 5)
 		{
-			/*32 is the difference between lower case letters and apper case letters*/
-			if (*c == key[i] || *c == key[i] + 32)
+			if (s[i] == s2[j] || s[i] == s3[j])
 			{
-				*c = 48 + value[i];
+				s[i] = s1[j];
 			}
+			j++;
 		}
-		c++;
+		i++;
 	}
-
-	return (cp);
-
+	return (ptr);
 }
