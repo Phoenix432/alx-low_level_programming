39493528d17f413a025354236fc83214ec5344a0
Phoenix432 <abdellahouben95@gmail.com>
2024-01-23 06:42:30 -0800

    0x08. C - Recursion task 7 advanced

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,3 +1,48 @@
 #include "main.h"
 
-int is_palindrome(char *s);
+/**
+ * leng - returns the length of string with the null char
+ * @a: pointer to the string
+ * Return: int
+ */
+
+
+int leng(char *a)
+{
+	int n = 0;
+
+	if (*a > '\0')
+		n += leng(a + 1) + 1;
+	return (n);
+}
+
+/**
+ * check - check process
+ * @a: string
+ * @l: length of the string
+ *
+ * Return: 1 or 0
+ */
+
+int check(char *a, int l)
+{
+	if (*a == 0)
+		return (1);
+	else if (*a != *(a + l))
+		return (0);
+	return (check(a + 1, l - 2));
+}
+
+/**
+ * is_palindrome - check if a string is a palindrome
+ * @s: string
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = leng(s);
+	return (check(s, l - 1));
+}
