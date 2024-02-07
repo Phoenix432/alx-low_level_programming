af342c7d2562997da884e81c8c168c677572cf15
Phoenix432 <abdellahouben95@gmail.com>
2024-02-06 17:35:00 -0800

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints linked list
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
+	}
+
+	return (c);
+}
